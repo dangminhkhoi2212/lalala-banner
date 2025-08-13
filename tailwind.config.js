96f5b21361a5bfc6317cf0b1a4ec9caec2e8119a
khoi_2212 <kkhoi600@gmail.com>
2025-08-13 10:14:36 +0700

    update(index): responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
 			fontFamily: {
 				dvn: ["DVN", "sans-serif"],
 			},
+			screens: {
+				// Landscape
+				landscape: { raw: "(orientation: landscape)" },
+				// Portrait
+				portrait: { raw: "(orientation: portrait)" },
+
+				// Landscape + min-width
+				"md-landscape": {
+					raw: "(min-width: 768px) and (orientation: landscape)",
+				},
+
+				// Landscape + max-width
+				"sm-landscape": {
+					raw: "(max-width: 640px) and (orientation: landscape)",
+				},
+			},
 		},
 	},
 	plugins: [],
